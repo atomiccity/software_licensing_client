0df23a6c595cf0178dfd2c1f89186c7d56da4ded
Michael R. Taylor <michael@atomic.city>
2025-01-06 10:24:17 -0600

    Added an "unlocked" LicenseClient for testing (or free software)

--- example/software_licensing_client_example.dart
+++ example/software_licensing_client_example.dart
@@ -1,18 +1,20 @@
 import 'package:software_licensing_client/software_licensing_client.dart';
 
 void main() async {
-  var licenseClient = LicenseClient(
+  var licenseClient = KeyedLicenseClient(
       storage: LocalLicenseStorage(),
       activator: WebApiLicenseActivator(host: 'myhost.com', path: '/validate'),
       publicKey: 'Your public key in PEM format');
   var localLicenseExists = await licenseClient.loadLocalLicense();
   if (!localLicenseExists) {
     var successfulActivation = await licenseClient.activateLicense(
-      key: 'Serial # given to user',
-      onError: (message) {
-        // Display error message to user
-      },
-    );
+        key: 'Serial # given to user',
+        onError: (message) {
+          // Display error message to user
+        },
+        onSuccess: () {
+          // Display success message to user
+        });
   }
 
   // Later in app
--- lib/src/license_client.dart
+++ lib/src/license_client.dart
@@ -3,17 +3,71 @@ import 'package:software_licensing_client/src/license_activator.dart';
 import 'package:software_licensing_client/src/license_io.dart';
 import 'package:software_licensing_client/src/license_storage.dart';
 
-class LicenseClient {
+abstract class LicenseClient {
+  Future<bool> loadLocalLicense();
+  Future<bool> activateLicense({
+    required String key,
+    String? username,
+    String? machineId,
+    required Function onSuccess,
+    required Function(String message) onError,
+  });
+  String? licensedTo();
+  bool hasValidLicense();
+  bool hasFeature(String feature);
+
+  const LicenseClient();
+}
+
+class UnlockedLicenseClient extends LicenseClient {
+  final String? licensedUserName;
+
+  const UnlockedLicenseClient({this.licensedUserName});
+
+  @override
+  Future<bool> activateLicense({
+    required String key,
+    String? username,
+    String? machineId,
+    required Function onSuccess,
+    required Function(String message) onError,
+  }) async {
+    return true;
+  }
+
+  @override
+  bool hasFeature(String feature) {
+    return true;
+  }
+
+  @override
+  bool hasValidLicense() {
+    return true;
+  }
+
+  @override
+  String? licensedTo() {
+    return licensedUserName;
+  }
+
+  @override
+  Future<bool> loadLocalLicense() async {
+    return true;
+  }
+}
+
+class KeyedLicenseClient extends LicenseClient {
   final String publicKey;
   final LicenseStorage storage;
   final LicenseActivator activator;
   SoftwareLicense? _license;
 
-  LicenseClient({required this.publicKey, required this.storage, required this.activator});
+  KeyedLicenseClient({required this.publicKey, required this.storage, required this.activator});
 
   /// Attempt to load a locally stored software license.
   ///
   /// Returns `true` if a license exists locally, `false` otherwise
+  @override
   Future<bool> loadLocalLicense() async {
     _license = await storage.loadLicense(publicKey);
     return _license != null;
@@ -23,6 +77,7 @@ class LicenseClient {
   ///
   /// Returns `true` if the key was valid and a license could be downloaded,
   /// `false` otherwise
+  @override
   Future<bool> activateLicense({
     required String key,
     String? username,
@@ -51,16 +106,19 @@ class LicenseClient {
   }
 
   /// Returns the user the software is licensed to, if available
+  @override
   String? licensedTo() {
     return _license?.licensedTo;
   }
 
   /// Returns `true` if the software license is available and valid
+  @override
   bool hasValidLicense() {
     return (_license != null) && _license!.validForHostName() && _license!.validForTime();
   }
 
   /// Checks to see if a feature has been licensed to use
+  @override
   bool hasFeature(String feature) {
     return (_license != null) &&
         _license!.validForTime() &&
